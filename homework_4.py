017e2c7abb882bc21c792446b17cde6b66603172
Yifan <xiayifanisme@gmail.com>
2018-12-13 21:33:35 +0100

    cleaned repo

--- homework_4.py
+++ homework_4.py
@@ -4,5 +4,9 @@ class Graph:
         self.adjacency_list = {}
 
     def read_input_file(self, file_name):
-        f = open()
+        with open(file_name, 'r') as f:
+            for edge in f:
+                end_points = list(map(int, edge.split()))
+                assert(len(end_points) == 2)
+
 
