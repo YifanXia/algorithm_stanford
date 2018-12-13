0b37756709e94781ee141f0ced12cb4ce24bf8be
Yifan <xiayifanisme@gmail.com>
2018-12-13 22:34:30 +0100

    added homework4

--- homework_4.py
+++ homework_4.py
@@ -2,11 +2,34 @@ class Graph:
     
     def __init__(self):
         self.adjacency_list = {}
+        self.reverse_list = {}
 
     def read_input_file(self, file_name):
         with open(file_name, 'r') as f:
             for edge in f:
                 end_points = list(map(int, edge.split()))
                 assert(len(end_points) == 2)
+                start = end_points[0]
+                fin = end_points[1]
+                try:
+                    self.adjacency_list[start] = self.adjacency_list[start] + [fin]
+                except KeyError:
+                    self.adjacency_list[start] = [fin]
+                
+                try:
+                    self.reverse_list[fin] = self.reverse_list[fin] + [start]
+                except KeyError:
+                    self.reverse_list[fin] = [start]
+                
+
+if __name__ == '__main__':
+
+    input_file = "SCC.txt"
+    graph = Graph()
+    graph.read_input_file(input_file)
+
+
+                
+                
 
 
