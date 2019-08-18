cad6414cec1add39d64863e587897f609d812822
Yifan Xia <xiayifanisme@gmail.com>
2019-08-18 21:24:17 +0200

    corrected some errors

--- Part2/scheduling_ratio.py
+++ Part2/scheduling_ratio.py
@@ -1,8 +1,9 @@
 from collections import namedtuple
 from typing import List, Dict, Tuple
 
-Task = namedtuple('Task', ['index', 'weight', 'length'])
+Task = namedtuple('Task', ['index', 'weight', 'length']) # add index to distinguish tasks with same weight and length
 #Schedule = List[Task]
+
 
 class Scheduler:
 
@@ -26,7 +27,7 @@ class Scheduler:
     def sort_tasks(tasks: List[Task]) -> List[Task]:
         scores = dict()
         for task in tasks:
-            scores[task] = task.weight - task.length
+            scores[task] = task.weight / task.length
         print("Scored computation finished.")
         sorted_scores = sorted(scores.items(), key = lambda task_score: task_score[1], reverse=True)
         return [task_score[0] for task_score in sorted_scores]
@@ -46,7 +47,6 @@ if __name__ == "__main__":
     print("Number of tasks: ", number_of_tasks)
     sorted_tasks = Scheduler.sort_tasks(tasks)
     print("Sorting completed.")
-    print(len(sorted_tasks))
     weighted_completion_time = Scheduler.compute_weighted_completion_time(sorted_tasks)
     print(weighted_completion_time)
 
