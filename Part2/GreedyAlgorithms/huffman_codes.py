7a3f389f1f80293b67a656f40be56f26d6d35064
Yifan Xia <xiayifanisme@gmail.com>
2020-01-15 22:08:18 +0100

    finished huffman

--- Part2/GreedyAlgorithms/huffman_codes.py
+++ Part2/GreedyAlgorithms/huffman_codes.py
@@ -4,11 +4,7 @@ import heapq
 
 @total_ordering
 class Node:
-    
-    def __init__(self, symbol, frequency, left = None, right = None):
-        
-        self._0 = left
-        self._1 = right
+    def __init__(self, symbol: Set[str], frequency: int):
         self.symbol = symbol
         self.frequency = frequency
         
@@ -17,26 +13,50 @@ class Node:
     
     def __eq__(self, other):
         return self.frequency == other.frequency
-        
     
-    
-def gather_forest(path: str) -> List[Node]:
+def get_initial_forest(path: str) -> List[Node]:
     with open(path, 'r') as file:
         num_nodes = int(file.readline())
         freqs = file.readlines()
         forest = [
-            Node(symbol=str(s), frequency=int(freqs[s].strip())) for s in range(num_nodes)
+            Node(symbol=[str(s)], frequency=int(freqs[s].strip())) for s in range(num_nodes)
         ]
         heapq.heapify(forest)
     return forest
 
 class HuffmanEncoder:
-    pass
+    def __init__(self, forest: List[Node]):
+        self.forest = forest
+        self.encoding = {node.symbol[0]: '' for node in forest}
+        self.frequencies = {node.symbol[0]: node.frequency for node in forest}
+    
+    def get_encoding(self):
+        while len(self.forest) > 1:
+            self.merge()
+            
+    def merge(self):
+        left_tree = heapq.heappop(self.forest)
+        right_tree = heapq.heappop(self.forest)
+        for symbol in left_tree.symbol:
+            self.encoding[symbol] += '0'
+        for symbol in right_tree.symbol:
+            self.encoding[symbol] += '1'
+        new_root = Node(
+            left_tree.symbol + right_tree.symbol,
+            left_tree.frequency + right_tree.frequency,
+        )
+        heapq.heappush(self.forest, new_root)
+            
+    
 
 if __name__ == "__main__":
     
-    PATH = 'C:/Users/Fenzhengrou/Documents/Algorithm_Stanford/algorithm_stanford/Part2/GreedyAlgorithms/huffman_test_2'
-    forest = gather_forest(PATH)
-    for tree in forest:
-        print(tree.symbol, tree.frequency)
+    PATH = 'C:/Users/Fenzhengrou/Documents/Algorithm_Stanford/algorithm_stanford/Part2/GreedyAlgorithms/huffman_test_3'
+    forest = get_initial_forest(PATH)
+    
+    huffman_encoder = HuffmanEncoder(forest)
+    huffman_encoder.get_encoding()
+    print(huffman_encoder.encoding)
+    encoding_lengths = [len(code) for code in huffman_encoder.encoding.values()]
+    print(min(encoding_lengths), max(encoding_lengths))
         
